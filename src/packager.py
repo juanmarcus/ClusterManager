37500a164daea4abd2be3d42a08106cd21203d1b
Juan Ibiapina <juanmarcus@gmail.com>
2009-09-25 16:58:31 -0300

    Better output for packager.py.

--- src/packager.py
+++ src/packager.py
@@ -7,20 +7,25 @@ def build_package():
     
     infilename = "config/packagefiles.txt"
     infile = open(infilename)
+    packagename = "clientpackage.tar.gz"
     
-    if os.path.exists("clientpackage.tar.gz"):
-        os.remove("clientpackage.tar.gz")
+    if os.path.exists(packagename):
+        print "removing previous package: %s" % packagename
+        os.remove(packagename)
     
-    tar = tarfile.open("clientpackage.tar.gz", "w:gz")
+    print "creating package: %s" % packagename
+    tar = tarfile.open(packagename, "w:gz")
     
-    os.chdir("./%s"%srcdir)
+    os.chdir("./%s" % srcdir)
     for line in infile:
         fline = line.strip()
         if fline:
             if fline.startswith("#"):
                 continue
             else:
+                print "addind file: %s" % fline
                 tar.add(fline)
+    print "closing package: %s" % packagename
     tar.close()
 
 if __name__ == "__main__":
